9022bdfcd5ebe38d7449e9b85aced8128ad53113
czx020 <34672693+czx020@users.noreply.github.com>
2018-09-05 17:42:21 +0800

    First Class
    2018.9.5

---
+++ 实验/Default.aspx
@@ -0,0 +1,58 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style>
+            div
+            {
+                text-align : center ;
+                }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Button ID="Button2" runat="server" Text="1到10的和" onclick="Button2_Click" />
+        <hr />
+        <asp:RadioButton ID="RadioButton1" runat="server" Text="蛇" GroupName="animal"/>
+        <asp:RadioButton ID="RadioButton2" runat="server" Text = "鸡儿" GroupName="animal"/>
+        <asp:RadioButton ID="RadioButton3" runat="server" Text = "狗" GroupName="animal"/>
+        <asp:RadioButton ID="RadioButton4"  runat="server" Text = "猪" GroupName="animal"/> <br />
+        <asp:Button ID="Button1" runat="server" Text="确定" onclick="Button1_Click" />
+        <asp:Label ID="Label1" runat="server" Text=""></asp:Label>
+        <hr />
+        <br />
+         请输入一个成绩：<asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <asp:Button ID="Button3" runat="server" Text="确定" onclick="Button3_Click" />
+         <br /><br />
+        <asp:Label ID="Label2" runat="server" Text="" ForeColor="red"></asp:Label>
+         <br /><br />
+        <asp:RadioButton ID="RadioButton5" runat="server" Text="不及格" GroupName="score" />
+        <asp:RadioButton ID="RadioButton6" runat="server" Text="及格" GroupName="score"/>
+        <asp:RadioButton ID="RadioButton7" runat="server" Text="良好" GroupName="score"/>
+        <asp:RadioButton ID="RadioButton8" runat="server" Text="优秀" GroupName="score"/>    
+    </div>
+    <hr />
+    <div>你觉得谁是弟弟？<br /><br />
+        <asp:RadioButton ID="RadioButton9" runat="server" Text="张晓刚" GroupName="didi" AutoPostBack = true
+            oncheckedchanged="RadioButton9_CheckedChanged" />
+        <asp:RadioButton ID="RadioButton10" runat="server" Text="薛尚" GroupName="didi" AutoPostBack = true
+            oncheckedchanged="RadioButton10_CheckedChanged"/>
+        <asp:RadioButton ID="RadioButton11" runat="server" Text="黄梓航" GroupName="didi" AutoPostBack = true
+            oncheckedchanged="RadioButton11_CheckedChanged"/>
+        <asp:RadioButton ID="RadioButton12" runat="server" Text="杨伟庭" GroupName="didi" AutoPostBack = true
+            oncheckedchanged="RadioButton12_CheckedChanged"/><br /><br />
+        <asp:Label ID="Label3" runat="server" Text=""></asp:Label><br /><br />
+        <asp:Button ID="Button4" runat="server" Text="是他!" OnClick ="Button_Click" Height="56px" Width="153px" />
+            
+    </div>
+    </form>
+</body>
+</html>
+    </div>
+    </form>
+</body>
+</html>
---
+++ 实验/Default.aspx.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        int a ;
+        a = 0;
+        for (int i = 1; i <= 10; i++) 
+        {
+            a += i;
+        }
+        Response.Write("1到10的和为：" + a.ToString());
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+    /*Label1.Text = "你选择的是:弟弟";*/
+        if (RadioButton1.Checked) { Label1.Text = "你选中了：" + RadioButton1.Text; }
+        if (RadioButton2.Checked) { Label1.Text = "你选中了：" + RadioButton2.Text; }
+        if (RadioButton3.Checked) { Label1.Text = "你选中了：" + RadioButton3.Text; }
+        if (RadioButton4.Checked) { Label1.Text = "你选中了：" + RadioButton4.Text; }
+    }
+
+    protected void Button3_Click(object sender, EventArgs e)
+    {
+        double s_num = -1;
+
+        RadioButton5.Checked = false;
+        RadioButton6.Checked = false;
+        RadioButton7.Checked = false;
+        RadioButton8.Checked = false;
+
+        try { s_num = double.Parse(TextBox1.Text); }
+        catch { }
+
+        if (s_num < 0 || s_num > 120)
+        {
+            Label2.Text = ("请输入正确的成绩");
+        }
+        else 
+        {
+            if (s_num < 60) { RadioButton5.Checked = true; }
+            if (s_num >= 60 && s_num < 80) { RadioButton6.Checked = true; }
+            if (s_num >=80 && s_num < 100) { RadioButton7.Checked = true; }
+            if (s_num >=100 && s_num < 120) { RadioButton8.Checked = true; }
+
+        }
+    }
+    protected void RadioButton9_CheckedChanged(object sender, EventArgs e)
+    {
+        if (RadioButton9.Checked == true)
+        {
+            Label3.Text = "你选择了" + RadioButton9.Text;
+        }
+    }
+    protected void RadioButton10_CheckedChanged(object sender, EventArgs e)
+    {
+        if (RadioButton10.Checked == true)
+        {
+            Label3.Text = "你选择了" + RadioButton10.Text;
+        }
+    }
+    protected void RadioButton11_CheckedChanged(object sender, EventArgs e)
+    {
+        if (RadioButton11.Checked == true)
+        {
+            Label3.Text = "你选择了" + RadioButton11.Text;
+        }
+    }
+    protected void RadioButton12_CheckedChanged(object sender, EventArgs e)
+    {
+        if (RadioButton12.Checked == true)
+        {
+            Label3.Text = "你选择了" + RadioButton12.Text;
+        }
+    }
+    protected void Button_Click(object sender, EventArgs e)
+    {
+        if (RadioButton9.Checked == false && RadioButton10.Checked == false && RadioButton11.Checked == false && RadioButton12.Checked == false)
+        {
+            Response.Write("<script>alert('请选择答案')</script>");
+        }
+        else if (RadioButton9.Checked == true)
+        {
+            Response.Write("<script>alert('没错，张晓刚就是个弟弟')</script>");
+        }
+        else if (RadioButton10.Checked == true)
+            {
+                Response.Write("<script>alert('没错，薛尚就是个弟弟')</script>");
+            }
+        else if (RadioButton11.Checked == true)
+            {
+                Response.Write("<script>alert('没错，黄梓航就是个弟弟')</script>"); 
+            }
+        else if (RadioButton12.Checked == true)
+            {
+                Response.Write("<script>alert('没错，杨伟庭就是个弟弟')</script>");    
+            }
+        }
+    }
