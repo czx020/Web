a306d1a5695f5d47f3f8819f4d156860906f3b05
czx020 <34672693+czx020@users.noreply.github.com>
2018-09-19 11:41:27 +0800

    Add files via upload

---
+++ 2018.9.19/Default.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div id="start" runat=server>
+        起始:
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        结束:
+        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        <br /><br />
+        <asp:Button ID="Button1" runat="server" Text="计算" onclick="Button1_Click" />
+        <br /><br />
+        <asp:Label ID="Label1" runat="server" Text=""></asp:Label>
+    </div>
+    <br />
+    <br />
+    <div id="连加求和">
+        起始:
+        <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+        结束:
+        <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+        <br /><br />
+        <asp:Button ID="Button2" runat="server" Text="计算" onclick="Button2_Click" />
+        <br /><br />
+        <asp:Label ID="Label2" runat="server" Text=""></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ 2018.9.19/Default.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        int b_start = int.Parse(TextBox1.Text);
+        int b_end = int.Parse(TextBox2.Text);
+        int b_test;
+        Boolean b_result;
+
+        string str_result = "质数有：" ;
+
+        for ( b_test = b_start; b_test <= b_end; b_test++)
+        {
+            b_result = zhishu(b_test);
+            if (b_result == true)
+            {
+                str_result += b_test + ","; 
+            }
+ 
+        }
+        Label1.Text = str_result;
+    }
+    protected Boolean zhishu(int be_test)
+    {
+        Boolean _result = true;
+        for (int i = be_test / 2; i >= 2; i--)
+        {
+            if (be_test % i == 0) { _result = false; break; }
+        }
+        return _result;
+    }
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        int b_start = int.Parse(TextBox1.Text);
+        int b_end = int.Parse(TextBox2.Text);
+        while()
+
+    }
+}
