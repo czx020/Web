b62246e2c85cf24b5759af49d29bec94b0c71a6f
崔泽贤 <34672693+czx020@users.noreply.github.com>
2019-03-19 14:19:07 +0800

    Add files via upload

---
+++ teacher/tabshili.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="tabshili.aspx.cs" Inherits="tabshili" Debug="true" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <script src="jquery-1.4.1.js" type="text/javascript"></script>
+    <script type="text/javascript">
+        $(document).ready(function () {       
+            $("#tabfirst li").each(function (index) {
+                $(this).mouseover(function () {
+                    $("#tabfirst>li.first").removeClass("#tabfirst first");
+                    $("#show>div.one").removeClass("#show one");
+                    //alert(index);
+                    // $("div:eq(" + index + ")").addClass("divcur");
+                    $("#show>div").eq(index).addClass("#show one");
+                    $("#tabfirst>li").eq(index).addClass("#tabfirst first");
+                });
+            });
+        });     
+    </script>
+    <link href="StyleSheet.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <ul id="tabfirst">
+            <li class="first">蛋糕</li>
+            <li>面包</li>
+            <li>巧克力</li>
+        </ul>
+        <div id="show">
+            <asp:Panel ID="Panel1" runat="server" class="one" >
+            </asp:Panel>
+            <asp:Panel ID="Panel2" runat="server">
+            </asp:Panel>
+            <asp:Panel ID="Panel3" runat="server">
+            </asp:Panel>
+        </div>
+
+    </form>
+</body>
+</html>
---
+++ teacher/tabshili.aspx.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.OleDb;
+
+public partial class tabshili : System.Web.UI.Page
+{
+    Image imgshangpin;
+    HyperLink nameshangping;
+    Panel panelshangpin;
+
+    string str_connection = "Provider=Microsoft.Jet.OLEDB.4.0; Data Source=";
+    string str_sourcefile = "tianpingdian.mdb";
+    OleDbConnection cnn;
+    OleDbCommand cmd;
+    OleDbDataReader datar;
+    string str_sql;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string str_conn = str_connection + MapPath(str_sourcefile);
+        cnn = new OleDbConnection(str_conn);
+        cnn.Open();
+        str_sql = "select   top 4 * from 商品明细  where 产品类别='蛋糕'";
+        cmd = new OleDbCommand(str_sql, cnn);
+        datar = cmd.ExecuteReader();
+        while (datar.Read()) { 
+            imgshangpin = new Image();
+            nameshangping = new HyperLink();
+            panelshangpin = new Panel();       
+            string name =datar["产品图片"].ToString();
+            imgshangpin.ImageUrl="image\\"+name;
+            nameshangping.Text = datar["产品名称"].ToString();
+            nameshangping.NavigateUrl = "tabshili.aspx";
+           panelshangpin.Controls.Add(imgshangpin);
+           panelshangpin.Controls.Add(nameshangping);
+           panelshangpin.CssClass="imgdiv";
+            Panel1.Controls.Add(panelshangpin);
+            }
+        str_sql = "select   top 4 * from 商品明细  where 产品类别='面包'";
+        cmd = new OleDbCommand(str_sql, cnn);
+        datar = cmd.ExecuteReader();
+        while (datar.Read()) { 
+            imgshangpin = new Image();
+            nameshangping = new HyperLink();
+            panelshangpin = new Panel();       
+            string name =datar["产品图片"].ToString();
+            imgshangpin.ImageUrl="image\\"+name;
+            nameshangping.Text = datar["产品名称"].ToString();
+            nameshangping.NavigateUrl = "tabshili.aspx";
+           panelshangpin.Controls.Add(imgshangpin);
+           panelshangpin.Controls.Add(nameshangping);
+           panelshangpin.CssClass = "imgdiv";
+           Panel2.Controls.Add(panelshangpin);
+
+        }
+            str_sql = "select   top 4 * from 商品明细  where 产品类别='巧克力'";
+        cmd = new OleDbCommand(str_sql, cnn);
+        datar = cmd.ExecuteReader();
+        while (datar.Read()) { 
+            imgshangpin = new Image();
+            nameshangping = new HyperLink();
+            panelshangpin = new Panel();       
+            string name =datar["产品图片"].ToString();
+            imgshangpin.ImageUrl="image\\"+name;
+            nameshangping.Text = datar["产品名称"].ToString();
+            nameshangping.NavigateUrl = "tabshili.aspx";
+           panelshangpin.Controls.Add(imgshangpin);
+           panelshangpin.Controls.Add(nameshangping);
+           panelshangpin.CssClass = "imgdiv";
+           Panel3.Controls.Add(panelshangpin);
+            }
+         }
+     }   
+                           
+     
